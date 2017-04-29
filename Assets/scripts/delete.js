83d83b8d54d0c3e3b92a84a69624139ffc426513
SaulSaandovalM <sauldevelop@gmail.com>
2017-04-29 11:46:00 -0500

    screamer

---
+++ Assets/scripts/delete.js
@@ -0,0 +1,11 @@
+﻿var ghost :GameObject; 
+function Start(){
+  ghost = GameObject.Find("pylon"); 
+}
+ 
+function OnTriggerEnter (other : Collider) {   
+
+  if(other.gameObject.tag == "Player")
+
+  Destroy(ghost);  
+}
